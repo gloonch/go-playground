57c4794436dab3f7661af24a26abdfe4f7e88dd8
gloonch <mahdihadian@hotmail.com>
2024-04-28 20:20:40 +0330

    Refactored todo-cli.go

--- todo-cli/todo-cli.go
+++ todo-cli/todo-cli.go
@@ -5,7 +5,16 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strconv"
 )
+
+type User struct {
+	ID       string
+	Email    string
+	Password string
+}
+
+var userStorage []User
 
 func main() {
 	fmt.Println("Hello to TODO app")
@@ -13,53 +22,113 @@ func main() {
 	command := flag.String("command", "no-command", "command to run")
 	flag.Parse()
 
-	scanner := bufio.NewScanner(os.Stdin)
-	//input: name
-	//input: category
-	//input: due date
-	if *command == "create-task" {
-		var name, duedate, category string
+	for {
+		runCommand(*command)
 
-		fmt.Println("Please enter the task title")
+		scanner := bufio.NewScanner(os.Stdin)
+		fmt.Println("Please enter another command")
 		scanner.Scan()
-		name = scanner.Text()
-
-		fmt.Println("Please enter the task category")
-		scanner.Scan()
-		category = scanner.Text()
-
-		fmt.Println("Please enter the task due date")
-		scanner.Scan()
-		duedate = scanner.Text()
-
-		fmt.Println("task: ", name, category, duedate)
-
-	} else if *command == "create-category" {
-		var title, color string
-
-		fmt.Println("Please enter the category title")
-		scanner.Scan()
-		title = scanner.Text()
-
-		fmt.Println("Please enter the category color")
-		scanner.Scan()
-		color = scanner.Text()
-
-		fmt.Println("category: ", title, color)
-	} else if *command == "register-user" {
-		var id, email, password string
-
-		fmt.Println("Please enter user email")
-		scanner.Scan()
-		email = scanner.Text()
-
-		fmt.Println("Please enter user password")
-		scanner.Scan()
-		password = scanner.Text()
-
-		id = email
-
-		fmt.Println("user: ", id, email, password)
+		*command = scanner.Text()
 	}
+
+	fmt.Printf("userStorage: %+v\n", userStorage)
+}
+
+func runCommand(command string) {
+	switch command {
+	case "create-task":
+		createTask()
+	case "create-category":
+		createCategory()
+	case "register-user":
+		registerUser()
+	case "login":
+		login()
+	case "exit":
+		os.Exit(0)
+	default:
+		fmt.Println("Command is not valid", command)
+	}
+
+}
+
+func createTask() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var name, duedate, category string
+
+	fmt.Println("Please enter the task title")
+	scanner.Scan()
+	name = scanner.Text()
+
+	fmt.Println("Please enter the task category")
+	scanner.Scan()
+	category = scanner.Text()
+
+	fmt.Println("Please enter the task due date")
+	scanner.Scan()
+	duedate = scanner.Text()
+
+	fmt.Println("task: ", name, category, duedate)
+
+}
+
+func createCategory() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var title, color string
+
+	fmt.Println("Please enter the category title")
+	scanner.Scan()
+	title = scanner.Text()
+
+	fmt.Println("Please enter the category color")
+	scanner.Scan()
+	color = scanner.Text()
+
+	fmt.Println("category: ", title, color)
+
+}
+
+func registerUser() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var id, email, password string
+
+	fmt.Println("Please enter user email")
+	scanner.Scan()
+	email = scanner.Text()
+
+	fmt.Println("Please enter user password")
+	scanner.Scan()
+	password = scanner.Text()
+
+	id = email
+
+	fmt.Println("user: ", id, email, password)
+
+	user := User{
+		ID:       strconv.Itoa(len(userStorage) + 1),
+		Email:    email,
+		Password: password,
+	}
+	userStorage = append(userStorage, user)
+
+}
+
+func login() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var id, email, password string
+
+	fmt.Println("Please enter user email")
+	scanner.Scan()
+	email = scanner.Text()
+
+	fmt.Println("Please enter user password")
+	scanner.Scan()
+	password = scanner.Text()
+
+	fmt.Println("user: ", id, email, password)
 
 }
